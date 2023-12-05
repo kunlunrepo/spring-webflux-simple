86ad28fc923f66a415765ea2f1367a3182721661
kunlunrepo <482976531@qq.com>
2023-12-05 16:52:02 +0800

    使用thymeleaf

--- src/main/java/com/msb/controller/PlayListController.java
+++ src/main/java/com/msb/controller/PlayListController.java
@@ -1,9 +1,11 @@
 package com.msb.controller;
 
+import com.msb.bean.PasswordDTO;
 import com.msb.bean.Song;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.thymeleaf.spring5.context.webflux.ReactiveDataDriverContextVariable;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -20,21 +22,20 @@ import java.util.List;
 @Controller
 public class PlayListController {
 
-    @RequestMapping("/play-list-view-ftl")
-    public Mono<String> getPlayList(final Model model) {
+    @RequestMapping("/play-list-view-thy")
+    public String getPlayList(final Model model) {
         List<Song> songs = new ArrayList<>();
         Song song = null;
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 1000; i++) {
             song = new Song(i, "张三"+i, "1001"+i, "专辑"+i);
             songs.add(song);
         }
         // 转化成响应式流
-        final Flux<Song> playListStream = Flux.fromIterable(songs);
+        Flux<Song> playListStream = Flux.fromIterable(songs)
+                .delayElements(Duration.ofMillis(500)); // 500ms产生一个数据
+        model.addAttribute("playList", new ReactiveDataDriverContextVariable(playListStream, 1, 1));
         // 渲染数据
-        return playListStream
-                .collectList()
-                .doOnNext(list-> model.addAttribute("playList", list))
-                .then(Mono.just("/freemarker/play-list-view"));
+        return "play-list-view";
     }
 
 }
